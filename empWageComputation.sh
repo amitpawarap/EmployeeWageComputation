fd69e625a05107a751c6bd8ec08c1f28d6be6d51
Amit Pawar <pawaramitaaa@gmail.com>
2020-07-05 23:03:26 +0530

    Attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,13 @@
 #welcome to Employee Wage Computation
 #!/bin/bash -x
 echo "welcome to Employee Wage Computation"
+
+Attendence=$(( $RANDOM % 2 + 1 ))
+if [ $Attendence -eq 1 ]
+then
+employ="Present"
+echo $employ
+else
+employ="Absent"
+echo $employ
+fi
